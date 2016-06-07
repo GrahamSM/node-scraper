7aa77bdc18caab3593e4144741d8b40b22d65c3d
graham <graham.sm94@gmail.com>
2016-06-07 15:16:53 -0400

    Fixed bugs, and implemented proper callback using async

--- scraper_module.js
+++ scraper_module.js
@@ -2,35 +2,26 @@ const request = require('request');
 const cheerio = require('cheerio');
 const fs = require('fs');
 const wstream = fs.createWriteStream('myOutput.txt');
+const async = require('async');
 
+var url = 'http://substack.net/images/';
+var extension_pattern = /\.([0-9a-z]+)(?:[\?#]|$)/i;
 
+async.each(dive_in(url, extension_pattern), function(callback){
+  wstream.close();
+});
 
-
-request('http://substack.net/images/', function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    var html_arr = body.split('</tr>')
-    var $;
-    var match;
-    var ext
-    var extension_pattern = /\.([0-9a-z]+)(?:[\?#]|$)/i;
-    iterate_through(html_arr, extension_pattern, 'http://substack.net/images/');
-  }
-  else{
-    console.log(error);
-  }
-})
 
 function dive_in(url, regex){
   request(url, function(error,response,body){
-    if (!error && response.statusCode == 200) {
-      var html_arr = body.split('</tr>')
+    if (!error && response.statusCode == 200){
+      var html_arr = body.split('</tr>');
       iterate_through(html_arr, regex, url)
-    }
-    else{
+    } else{
       console.log(error);
-    }
-  })
-}
+    };
+  });
+};
 
 function iterate_through(html_arr, regex, url){
   for (var i = 0; i < html_arr.length; i++){
